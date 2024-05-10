614899fbdb01b532da110258279844d8a77fc8a1
mengliu1998 <604629@gmail.com>
2024-05-09 22:00:30 -0700

    Keep both simple_rag and configured rag

--- use_cases/rag.py
+++ use_cases/rag.py
@@ -1,16 +1,16 @@
 from typing import Any, List, Optional
 import dotenv
-
+import yaml
 
 from core.openai_client import OpenAIClient
 from core.generator import Generator
 from core.embedder import Embedder
+from core.documents_data_class import Document
 from core.data_components import (
     ToEmbedderResponse,
     RetrieverOutputToContextStr,
     ToEmbeddings,
 )
-from core.data_classes import Document
 
 from core.document_splitter import DocumentSplitter
 from core.string_parser import JsonParser
@@ -18,71 +18,51 @@ from core.component import Component, Sequential
 from core.retriever import FAISSRetriever
 from core.db import LocalDocumentDB
 
-from core.functional import generate_component_key
-
 dotenv.load_dotenv(dotenv_path=".env", override=True)
 
 
 # TODO: RAG can potentially be a component itsefl and be provided to the users
 class RAG(Component):
 
-    def __init__(self):
+    def __init__(self, settings: dict):
         super().__init__()
+        self.settings = settings
+        self.vectorizer_settings = self.settings["vectorizer"]
+        self.retriever_settings = self.settings["retriever"]
+        self.generator_model_kwargs = self.settings["generator"]
+        self.text_splitter_settings = self.settings["text_splitter"]
 
-        self.vectorizer_settings = {
-            "batch_size": 100,
-            "model_kwargs": {
-                "model": "text-embedding-3-small",
-                "dimensions": 256,
-                "encoding_format": "float",
-            },
-        }
-        self.retriever_settings = {
-            "top_k": 2,
-        }
-        self.generator_model_kwargs = {
-            "model": "gpt-3.5-turbo",
-            "temperature": 0.3,
-            "stream": False,
-        }
-        self.text_splitter_settings = {  # TODO: change it to direct to spliter kwargs
-            "split_by": "word",
-            "chunk_size": 400,
-            "chunk_overlap": 200,
-        }
-
-        vectorizer = Embedder(
+        self.vectorizer = Embedder(
             model_client=OpenAIClient(),
-            # batch_size=self.vectorizer_settings["batch_size"], #TODO: where to put the batch size control and how big can it go?
+            # batch_size=self.vectorizer_settings["batch_size"],
             model_kwargs=self.vectorizer_settings["model_kwargs"],
-            output_processors=ToEmbedderResponse(),
+            output_processors=Sequential(ToEmbedderResponse()),
         )
-        # TODO: check document splitter, how to process the parent and order of the chunks
         text_splitter = DocumentSplitter(
             split_by=self.text_splitter_settings["split_by"],
             split_length=self.text_splitter_settings["chunk_size"],
             split_overlap=self.text_splitter_settings["chunk_overlap"],
         )
-        self.data_transformer = Sequential(
+        data_transformer = Sequential(
             text_splitter,
             ToEmbeddings(
-                vectorizer=vectorizer,
+                vectorizer=self.vectorizer,
                 batch_size=self.vectorizer_settings["batch_size"],
             ),
         )
-        self.data_transformer_key = generate_component_key(self.data_transformer)
+        self.db = LocalDocumentDB(data_transformer=data_transformer)
         # initialize retriever, which depends on the vectorizer too
         self.retriever = FAISSRetriever(
             top_k=self.retriever_settings["top_k"],
             dimensions=self.vectorizer_settings["model_kwargs"]["dimensions"],
-            vectorizer=vectorizer,
+            vectorizer=self.vectorizer,
+            # db=self.db,
+            output_processors=RetrieverOutputToContextStr(deduplicate=True),
         )
-        self.retriever_output_processors = RetrieverOutputToContextStr(deduplicate=True)
-        # TODO: currently retriever will be applied on transformed data. but its not very obvious design pattern
-        self.db = LocalDocumentDB()
-
         # initialize generator
         self.generator = Generator(
+            model_client=OpenAIClient(),
+            output_processors=Sequential(JsonParser()),
             preset_prompt_kwargs={
                 "task_desc_str": r"""
 You are a helpful assistant.
@@ -95,22 +75,14 @@ Output JSON format:
     "answer": "The answer to the query",
 }"""
             },
-            model_client=OpenAIClient,
             model_kwargs=self.generator_model_kwargs,
-            output_processors=JsonParser(),
         )
-        self.tracking = {
-            "vectorizer": {"num_calls": 0, "num_tokens": 0}
-        }  # TODO: tracking of the usage can be added in default in APIClient component
+        self.tracking = {"vectorizer": {"num_calls": 0, "num_tokens": 0}}
 
     def build_index(self, documents: List[Document]):
         self.db.load_documents(documents)
-        self.map_key = self.db.map_data()
-        print(f"map_key: {self.map_key}")
-        self.data_key = self.db.transform_data(self.data_transformer)
-        print(f"data_key: {self.data_key}")
-        self.transformed_documents = self.db.get_transformed_data(self.data_key)
-        self.retriever.build_index_from_documents(self.transformed_documents)
+        self.db()  # transform the documents
+        self.retriever.set_chunks(self.db.transformed_documents)
 
     def generate(self, query: str, context: Optional[str] = None) -> Any:
         if not self.generator:
@@ -119,26 +91,19 @@ Output JSON format:
         prompt_kwargs = {
             "context_str": context,
         }
-        response = self.generator(input=query, prompt_kwargs=prompt_kwargs)
+        response = self.generator.call(input=query, prompt_kwargs=prompt_kwargs)
         return response
 
     def call(self, query: str) -> Any:
-        retrieved_documents = self.retriever(query)
-        # fill in the document
-        for i, retriever_output in enumerate(retrieved_documents):
-            retrieved_documents[i].documents = [
-                self.transformed_documents[doc_index]
-                for doc_index in retriever_output.doc_indexes
-            ]
-        # convert all the documents to context string
-
-        context_str = self.retriever_output_processors(retrieved_documents)
-
+        context_str = self.retriever(query)
         return self.generate(query, context=context_str)
 
 
 if __name__ == "__main__":
     # NOTE: for the ouput of this following code, check text_lightrag.txt
+    with open("./configs/rag.yaml", "r") as file:
+        settings = yaml.safe_load(file)
+    print(settings)
     doc1 = Document(
         meta_data={"title": "Li Yin's profile"},
         text="My name is Li Yin, I love rock climbing" + "lots of nonsense text" * 500,
@@ -151,15 +116,11 @@ if __name__ == "__main__":
         + "lots of more nonsense text" * 250,
         id="doc2",
     )
-    rag = RAG()
+    rag = RAG(settings)
     print(rag)
     rag.build_index([doc1, doc2])
     print(rag.tracking)
     query = "What is Li Yin's hobby and profession?"
 
     response = rag.call(query)
-
-    # print(f"execution graph: {rag._execution_graph}")
     print(f"response: {response}")
-    # print(f"subcomponents: {rag._components}")
-    rag.visualize_graph_html("my_component_graph.html")
