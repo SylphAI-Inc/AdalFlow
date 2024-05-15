497b7c3a6df631ba47fb022c0592f108d872b450
Li Yin <li.yin.gravity@gmail.com>
2024-05-14 23:58:27 -0700

    clean up db

--- core/db.py
+++ core/db.py
@@ -6,11 +6,10 @@ For cloud db, you can additionally add a manager to CRUD the data. These are dat
 use some functional components in lightrag to process the data.
 """
 
-from typing import List, Optional, Any, Callable
+from typing import List, Optional, Callable
 from core.component import Component
 from core.data_classes import Document
 
-from core.retriever import Retriever, RetrieverInputType, RetrieverOutputType
 from core.functional import generate_component_key, generate_readable_key_for_function
 
 """
@@ -38,25 +37,10 @@ class LocalDocumentDB:
     def __init__(
         self,
         documents: Optional[List[Document]] = None,
-        # TODO: rename to retriever_transformer: only used to transform documents to retriever input which it uses to build index with
-        # retriever_transformer: Optional[Component] = None,
-        # retriever: Optional[
-        #     Retriever
-        # ] = None,  # retriever can be stateless and should have build_index method, attach a retriever to the db
-        # retriever_output_processors: Optional[
-        #     Component
-        # ] = None,  # any output processors to be applied to the retrieved documents, such as deduplication if query expansion is used
     ):
-        # super().__init__()
         self.documents = documents if documents else []  # the original documents
         self.transformed_documents = {}  # transformed documents
         self.mapped_documents = {}  # mapped documents
-        # data_transformer = Sequential(DocumentSplitter())
-        # TODO: how is data_transformer fit into the pipeline?, should it do in-place transformation?
-        # self.retriever_transformer = retriever_transformer
-        # self.transformed_documents = []
-        # self.retriever = retriever
-        # self.retrieve_output_processors = retriever_output_processors
 
     def list_mapped_data_keys(self) -> List[str]:
         return list(self.mapped_documents.keys())
@@ -115,36 +99,5 @@ class LocalDocumentDB:
     def reset_documents(self):
         self.documents = []
 
-    # def _get_documents_to_use(self):
-    #     if self.transformed_documents:
-    #         return self.transformed_documents
-    #     return self.documents
-
-    # def build_retrieve_index(self):
-    #     documents_to_use = self._get_documents_to_use()
-    #     self.retriever.build_index_from_documents(documents_to_use)
-
-    # # TODO: allow better otuput type specification and type hinting
-    # def retrieve(self, query_or_queries: RetrieverInputType) -> Any:
-    #     documents_to_use = self._get_documents_to_use()
-
-    #     # check if the retriever has build index or not
-    #     if not self.retriever.indexed:
-    #         raise ValueError("Retriever is not indexed, please call build_index first")
-    #     response: RetrieverOutputType = self.retriever(query_or_queries)
-    #     # set the actual documents to the response
-    #     for i, output in enumerate(response):
-    #         # convert doc_indexes to doc_contents
-    #         response[i].documents = [
-    #             documents_to_use[doc_index] for doc_index in output.doc_indexes
-    #         ]
-    #     # if self.retrieve_output_processors:
-    #     #     response = self.retrieve_output_processors(response)
-    #     return response
-
-    # def run_retriever_transformer(self) -> None:
-    #     if self.retriever_transformer:
-    #         self.transformed_documents = self.retriever_transformer(self.documents)
-
     def __call__(self) -> Optional[List[Document]]:
-        pass
+        raise ValueError("LocalDocumentDB is not callable")
