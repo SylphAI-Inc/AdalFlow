8430dc465a66a0153c3f400155b7da093cdf52f8
Alleria <xiaoyigu@usc.edu>
2024-05-20 17:51:00 -0700

    rename the logger

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,7 +1,7 @@
 from .logger import BaseLogger
 
 # Initialize and expose the logger instance
-base_logger = BaseLogger().logger
+default_logger = BaseLogger().logger
 
 # Expose the base_logger and BaseLogger for easier import
-__all__ = ['base_logger', 'BaseLogger']
+__all__ = ['default_logger', 'BaseLogger']
