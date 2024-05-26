b1a67a0f64996678898e243b2b1f787dc93ed5ce
Alleria <xiaoyigu@usc.edu>
2024-05-25 21:27:24 -0700

    adjust the .py

--- docs/source/insert_labels.py
+++ docs/source/insert_labels.py
@@ -17,4 +17,3 @@ if __name__ == "__main__":
     # Specify the directories you want to process
     add_reference_labels("./source/apis/core")
     add_reference_labels("./source/apis/components")
-    add_reference_labels("./source/apis/utils")
