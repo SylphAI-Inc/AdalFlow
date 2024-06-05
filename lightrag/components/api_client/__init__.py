a5a3b91b7af5a1e4a57af27f40bd2ce279af0a49
Li Yin <li.yin.gravity@gmail.com>
2024-06-04 23:50:50 -0700

    fix conditional package dependencides

--- lightrag/components/api_client/__init__.py
+++ lightrag/components/api_client/__init__.py
@@ -1,5 +1,24 @@
-from .openai_client import *
-from .groq_client import *
-from .anthropic_client import *
-from .transformers_client import *
-from .google_client import *
+try:
+    from .openai_client import *
+except ImportError:
+    pass
+try:
+    from .groq_client import *
+except ImportError:
+    pass
+try:
+    from .anthropic_client import *
+except ImportError:
+    pass
+try:
+    from .transformers_client import *
+except ImportError:
+    pass
+try:
+    from .google_client import *
+except ImportError:
+    pass
+# from .groq_client import *
+# from .anthropic_client import *
+# from .transformers_client import *
+# from .google_client import *
