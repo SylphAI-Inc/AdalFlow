78cbe27d39250164919107353db364c50eaa9354
Alleria <xiaoyigu@usc.edu>
2024-06-07 20:55:13 -0700

    update poetry dependencies

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,6 +14,7 @@ import os
 import sys
 
 
+sys.path.insert(0, os.path.abspath('../../'))
 sys.path.insert(0, os.path.abspath("../../lightrag"))
 # # need to insert the paths
 # for dir in os.walk('../../lightrag'):
