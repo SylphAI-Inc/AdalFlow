78e52ba3493dc9c2639c01fd5a0a77ac3045906e
Alleria <xiaoyigu@usc.edu>
2024-07-05 15:55:08 -0700

    remove unnecessary code

--- docs/source/remove_files.py
+++ docs/source/remove_files.py
@@ -30,38 +30,6 @@ def remove_file(directory: str):
     except Exception:
         print(f"No {name} to remove in {directory}")
 
-    # remove api files to avoid showing duplicated section
-
-    # target_files = [
-    #     "components.model_client.openai_client.rst",
-    #     "components.retriever.faiss_retriever.rst",
-    #     "components.reasoning.chain_of_thought.rst",
-    #     "components.model_client.groq_client.rst",
-    #     "components.retriever.bm25_retriever.rst",
-    #     "components.model_client.google_client.rst",
-    #     "components.model_client.transformers_client.rst",
-    #     "components.model_client.utils.rst",
-    #     "components.retriever.llm_retriever.rst",
-    #     "components.agent.react.rst",
-    #     "components.model_client.anthropic_client.rst",
-    #     "components.output_parsers.outputs.rst",
-    #     "components.model_client.cohere_client.rst",
-    #     "components.retriever.reranker_retriever.rst",
-    #     "components.data_process.data_components.rst",
-    #     "components.data_process.text_splitter.rst",
-    #     "components.memory.memory.rst",
-    #     # "components.retriever.postgres_retriever.rst",
-    # ]
-    # try:
-    #     for filename in os.listdir(directory):
-    #         if filename in target_files:
-    #             filepath = os.path.join(directory, filename)
-    #             os.remove(filepath)
-    #             print(f"{filepath} is removed")
-    # except Exception:
-    #     print(f"No target files to remove in {directory}")
-
-
 remove_file("./source/apis/components")
 remove_file("./source/apis/core")
 remove_file("./source/apis/eval")
