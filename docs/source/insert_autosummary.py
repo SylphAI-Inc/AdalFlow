37923f486574e0e90e455d041270d391c1d7eea4
Li Yin <li.yin.gravity@gmail.com>
2024-07-05 15:46:37 -0700

    fix the test for log config

--- docs/source/insert_autosummary.py
+++ docs/source/insert_autosummary.py
@@ -10,7 +10,6 @@ def list_all_files(directory):
             for filename in filenames:
                 middle = os.path.relpath(root, directory)
                 print(f"middle: {middle}")
-                # files.append(os.path.join(root, filename))
                 if middle != ".":
                     files.add(os.path.join(middle, filename))
                 else:
@@ -22,17 +21,10 @@ def list_all_files(directory):
 
 
 def generate_rst_for_module(module_full_name, module, output_dir):
-    # Initialize the content with the automodule directive
-    # content = f".. automodule:: {module_name}\n"
-    # content += "   :members:\n"
-    # content += "   :undoc-members:\n"
-    # content += "   :show-inheritance:\n\n"
-    # read the initial file and save the first five lines
+
     rst_filepath = os.path.join(output_dir, module_full_name + ".rst")
-    # first_five_lines = []
-    # with open(rst_filepath, "r") as file:
-    #     for i in range(5):
-    #         first_five_lines.append(file.readline())
+
+    # insert the following summary after the automodule directive
 
     content = "\n\n"
 
@@ -68,9 +60,7 @@ def generate_rst_for_module(module_full_name, module, output_dir):
 
 def generate_autosummary_docs(src_dir, dest_dir):
     print(f"Generating autosummary docs for {src_dir} to {dest_dir}")
-    files = os.listdir(src_dir)
     module = src_dir.split("/")[-1]
-    print(f"files: {files}")
     if not os.path.exists(dest_dir):
         os.makedirs(dest_dir)
 
@@ -89,38 +79,11 @@ def generate_autosummary_docs(src_dir, dest_dir):
             module_full_name = f"{module}.{file_to_module}"
             print(f"module_full_name: {module_full_name}")
             module_dir = dest_dir
-            print(f"module_dir: {module_dir}")
             # spec and load the module
             spec = importlib.util.spec_from_file_location(module_full_name, code_path)
-            print(f"spec: {spec}")
             module = importlib.util.module_from_spec(spec)
-            print(f"module: {module}")
             spec.loader.exec_module(module)
             generate_rst_for_module(module_full_name, module, module_dir)
-        #         module_path = os.path.relpath(
-        #             os.path.join(root, file), src_dir
-        #         ).replace(os.sep, ".")
-        #         submodules = module_path.replace(".py", "")
-        #         sub
-        #         # module_name should be
-        #         module_name = f"{module}.{file.}
-
-        #         # Load the module
-        #         spec = importlib.util.spec_from_file_location(
-        #             module_name, os.path.join(root, file)
-        #         )
-        #         module = importlib.util.module_from_spec(spec)
-        #         spec.loader.exec_module(module)
-
-        #         # Generate the .rst file for the module
-        #         generate_rst_for_module(module_name, module, dest_dir)
-
-
-# if __name__ == "__main__":
-#     src_dir = "path/to/your/python/modules"  # Adjust this path
-#     dest_dir = "path/to/your/docs/source/_autosummary"  # Adjust this path
-
-#     generate_autosummary_docs(src_dir, dest_dir)
 
 
 if __name__ == "__main__":
